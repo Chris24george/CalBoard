4562cea2800613c6575cb005d1ca3e2a3eb4fbfa
chris384g <chris@384.co>
2024-12-21 10:32:30 -0800

    Initial Commit

---
+++ CalBoard/CalBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalBoardApp.swift
+//  CalBoard
+//
+//  Created by Chris George on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CalBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
